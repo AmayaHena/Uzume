fb252c454146d4d6ad39ad187cceb422f2b8b3da
Amaya <guillaume.le-berre@epitech.eu>
2020-03-29 09:30:45 +0200

    [UP] char not int

--- src/parsing/Char.hs
+++ src/parsing/Char.hs
@@ -1,43 +1,41 @@
 --
--- PERSONAL PROJECT, 2020
+-- EPITECH PROJECT, 2020
 -- Uzume
 -- File description:
--- char
+-- Char
 --
 
 module Char
-( isDigit,
-  listIsDigit
+( isChar,
+  listIsChar
 ) where
 
 -- look at the current String and return
--- True if it contains only numbers, False otherwise
--- error case, return False
--- ex : True = isDigit "12345"
--- ex : False = isDigit "1a2b3c4d5"
+-- True if it contains only char, False otherwise
+-- ex : True = isChar "abcde"
+-- ex : False = isChar "a1b2c3d4e"
 
-isDigit :: [Char] -> Bool
-isDigit [] = False
-isDigit s = case reads s :: [(Integer, String)] of
-    [(_, "")] -> True
-    _         -> False
+isChar :: String -> Bool
+isChar [] = True
+isChar (x:xs)
+    | x >= 'a' && x <= 'z' = isChar xs
+    | otherwise = False
 
 ---
 
 -- look at the current String and return
--- True if it contains only numbers, False otherwise
--- error case, return False
--- ex : True = listIsDigit ["34","56"]
--- ex : False = listIsDigit ["34","5a6"]
+-- True if it contains only char, False otherwise
+-- ex : True = listIsDigit ["ab","cd"]
+-- ex : False = listIsDigit ["a1b2","cd"]
 
 followList :: [String] -> Int -> Bool
 followList l n = case (n > 0) of
     True -> do
-        case (isDigit $ l !! (n - 1)) of
+        case (isChar $ l !! (n - 1)) of
             True -> followList l (n - 1)
             False -> False
     False -> True
 
-listIsDigit :: [String] -> Bool
-listIsDigit [] = False
-listIsDigit l = followList l (length l)
+listIsChar :: [String] -> Bool
+listIsChar [] = False
+listIsChar l = followList l (length l)
