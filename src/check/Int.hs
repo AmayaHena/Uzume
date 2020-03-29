4dc6748e891c31fec69205bdc405fe90cf407d1d
Amaya <guillaume.le-berre@epitech.eu>
2020-03-29 10:01:18 +0200

    [UP] checksort

--- src/check/Int.hs
+++ src/check/Int.hs
@@ -9,7 +9,8 @@ module CInt
 ( inDiff,
   outDiff,
   doMod,
-  doDiv
+  doDiv,
+  checkSort
 ) where
 
 inDiff :: Int -> Int -> Int -> Bool
@@ -18,14 +19,34 @@ inDiff v min max
     | v > min && v < max = True
     | otherwise = False
 
+---
+
 outDiff :: Int -> Int -> Int -> Bool
 outDiff v min max
     | min > max = False
     | v >= min && v <= max = True
     | otherwise = False
 
+---
+
 doDiv :: Int -> Int -> Int
 doDiv a b = a `div` b
 
+---
+
 doMod :: Int -> Int -> Int
 doMod a b = a `mod` b
+
+---
+
+checkDigit :: [Int] -> Int -> Bool
+checkDigit (l:ls) a = case (a < length (l:ls)) of
+    True -> case (a > 0) of
+        True -> case (l <= (head ls)) of
+            True -> checkDigit ls (a - 1)
+            False -> False
+        False -> True
+    False -> checkDigit (l:ls) (a - 1)
+
+checkSort :: [Int] -> Bool
+checkSort l = checkDigit l (length l)
