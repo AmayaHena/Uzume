23a48aeee4d6d918809c9fe22fc86af262fd340b
Amaya <guillaume.le-berre@epitech.eu>
2020-03-29 10:12:40 +0200

    [ADD] op for char

---
+++ src/operation/Char.hs
@@ -0,0 +1,29 @@
+--
+-- EPITECH PROJECT, 2020
+-- Uzume
+-- File description:
+-- Char
+--
+
+module OChar
+( cToI,
+  lStrTOlInt
+) where
+
+cToI :: [Char] -> Int
+cToI [] = 0
+cToI a = read a
+
+---
+
+convertSTOI :: [[Char]] -> Int -> [Int]
+convertSTOI [] a = []
+convertSTOI (l:ls) a = case (a > length (l:ls)) of
+    False -> case (a > 0) of
+        True -> (cToI l: (convertSTOI ls (a - 1)) )
+        False -> []
+    True -> convertSTOI (l:ls) (a - 1)
+
+lStrTOlInt :: [String] -> [Int]
+lStrTOlInt [] = ([])
+lStrTOlInt a = (convertSTOI a (length a))
