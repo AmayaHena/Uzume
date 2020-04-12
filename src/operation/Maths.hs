f85c89bceec078fd66943da6701400c1edce0c49
Amaya <guillaume.le-berre@epitech.eu>
2020-04-12 19:23:13 +0200

    [UP] comment

--- src/operation/Maths.hs
+++ src/operation/Maths.hs
@@ -8,9 +8,13 @@
 module Maths
 ( average
 ) where
-import Data.List
+
+-- evaluate the average of a list of Int
+-- error case, return 0
+-- ex : average [10, 20] = 15
 
 average :: [Int] -> Int
+average [] = 0
 average l = sum [l !! i | i <- take k [0,1..]] `div` k
     where
         k = length l
